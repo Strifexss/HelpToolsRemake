c071558b5eeb88e6795cf07d34bd84ef23bc069e
Strifexss <rocombole0007@gmail.com>
2023-10-08 20:45:45 -0300

    Links Organizados

--- src/app/paginas/components/layout/PaginasArray.tsx
+++ src/app/paginas/components/layout/PaginasArray.tsx
@@ -1,5 +1,5 @@
-import { Home, Download, Library, Store } from "lucide-react";
-import { RotaConhecimentos, RotaDownloads, RotaHome, RotaSoftStore } from "@/app/hooks/useRotas";
+import { Home, Download, Library, Store, Triangle, Folder, Headphones, Paperclip } from "lucide-react";
+import { RotaConhecimentos, RotaDownloads, RotaHome, RotaSoftStore, RotaTreinamento, ChatSupervisao, RotaConhecimentosConteudo, RotaDocumentacao, RotaSuporteComercial } from "@/app/hooks/useRotas";
 import Buttons from "./asideComponents/Buttons";
 import {usePathname} from "next/navigation"
 
@@ -36,11 +36,39 @@ const ArrayDestacados = [
     Click: RotaSoftStore(),
     InPage: rotaNome === "/paginas/SoftStore"
   },
+  {
+    Conteudo: "Treinamento",
+    hasBackground: false,
+    Icone: <Triangle color={`${rotaNome === "/paginas/Treinamento" ? "orange" : "white"}`} />,
+    Click: RotaTreinamento(),
+    InPage: rotaNome === "/paginas/Treinamento"
+  },
+  {
+    Conteudo: "Documentação",
+    hasBackground: false,
+    Icone: <Folder color={`${rotaNome === "/paginas/Documentacao" ? "orange" : "white"}`} />,
+    Click: RotaDocumentacao(),
+    InPage: rotaNome === "/paginas/Documentacao"
+  },
+  {
+    Conteudo: "Suporte Comercial",
+    hasBackground: false,
+    Icone: <Headphones color={`${rotaNome === "/paginas/SuporteComercial" ? "orange" : "white"}`} />,
+    Click: RotaSuporteComercial(),
+    InPage: rotaNome === "/paginas/SuporteComercial"
+  },
+  {
+    Conteudo: "Chat Supervisão",
+    hasBackground: false,
+    Icone: <Paperclip color={`${rotaNome === "/paginas/ChatSupervisao" ? "orange" : "white"}`} />,
+    Click: ChatSupervisao(),
+    InPage: rotaNome === "/paginas/ChatSupervisao"
+  },
 ];
 
 return (
 
-<section className="w-full gap-1">
+<section className="w-full flex flex-col gap-1">
   {ArrayDestacados.map((x) => {
     return (
       <Buttons
--- src/app/paginas/components/layout/asideComponents/ButtonsSection.tsx
+++ src/app/paginas/components/layout/asideComponents/ButtonsSection.tsx
@@ -1,32 +1,20 @@
 "use client"
 import Buttons from "./Buttons"
-import { Download, Library, LucideLibrary, Headphones, Book, Triangle, Home, User2, Store } from "lucide-react"
+import { User2 } from "lucide-react"
 import { UserModel } from "@/app/Models/UserModel"
 import ModalUSuario from "./ModalUsuario"
 import { useState } from "react"
-import { usePathname } from "next/navigation"
 import  PaginasDestacadas  from "../PaginasArray"
-import { RotaDocumentacao, RotaSuporteComercial, RotaTreinamento, ChatSupervisao } from "@/app/hooks/useRotas"
 
 export default function ButtonsSection() {
     
     const Usuario = new UserModel()
     const [showModal, setShowModal] = useState<boolean>(false)
 
-    const RotaDocumento = RotaDocumentacao()
-    const RotaTreino = RotaTreinamento()
-    const RotaSuportecomercial = RotaSuporteComercial()
-    const RotaChat = ChatSupervisao()
     return(
         <section className="w-full h-full flex flex-col gap-4 2xl:gap-6">
             <section className="w-full gap-1">
                 <PaginasDestacadas />
-            </section>
-            <section className="w-full gap-1">
-                <Buttons Click={() => RotaTreino()} Conteudo="Treinamento" Icone={<Triangle color="white"/>} key={"Treinamento"}/>
-                <Buttons Click={() => RotaDocumento()} Conteudo="Documentação" Icone={<Book color="white"/>} key={"Documentação"}/>
-                <Buttons Click={() => RotaSuportecomercial()} Conteudo="SuporteComercial" Icone={<Headphones color="white"/>} key={"SuporteComercial"}/>
-                <Buttons Click={() => RotaChat()} Conteudo="Chat Supervisão" Icone={<Library color="white"/>} key={"Chat Supervisão"}/>
             </section>
             <section className="w-full h-full flex flex-col justify-end items-center">
                 { showModal &&
